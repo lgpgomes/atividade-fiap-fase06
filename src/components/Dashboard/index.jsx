7fe52d6dc248a50ec49905d6654acc58cb836685
Gabriel Reis <gabrielreiscunha6@gmail.com>
2024-09-30 12:38:51 -0300

    estruturação react-router-dom

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,5 +1,5 @@
 // import { useState} from "react";
-import { Routes, Route, Navigate, useNavigate} from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Sidebar from "../Sidebar";
 import Monitoramento from "../../pages/Monitoramento";
 import PraticasAgricolas from "../../pages/PraticasAgricolas";
@@ -7,6 +7,11 @@ import Mercado from "../../pages/Mercado";
 import PlantiosRecomendados from "../../pages/PraticasAgricolas/PlantiosRecomendados";
 import CadastrarPlantios from "../../pages/PraticasAgricolas/CadastrarPlantios";
 import "./Dashboard.css";
+import CadastroLavoura from "../../pages/Monitoramento/CadastroLavoura";
+import DadosSolo from "../../pages/Monitoramento/DadosSolo";
+import PlantiosExistentes from "../../pages/Monitoramento/PlantiosExistentes";
+import VenderPlantio from "../../pages/Mercado/VenderPlantio";
+import VerPlantios from "../../pages/Mercado/VerPlantios";
 
 function Dashboard() {
   const navigator = useNavigate();
@@ -22,34 +27,66 @@ function Dashboard() {
       <Sidebar />
       <div className="content">
         <Routes>
+          {/* Redirecionamento inicial para Monitoramento */}
           <Route
             path="/"
             element={<Navigate to="/dashboard/monitoramento" />}
           />
-          <Route path="monitoramento" element={<Monitoramento />} />
+
+          {/* Monitoramento */}
+          <Route path="monitoramento" element={<Monitoramento />}>
+            {/* Rota de redirecionamento */}
+            <Route
+              index
+              element={
+                <Navigate to="/dashboard/monitoramento/cadastrar-lavoura" />
+              }
+            />
+            {/* Rotas de Monitoramento */}
+            <Route path="cadastrar-lavoura" element={<CadastroLavoura />} />
+            <Route path="dados-do-solo" element={<DadosSolo />} />
+            <Route
+              path="plantios-existentes"
+              element={<PlantiosExistentes />}
+            />
+          </Route>
+
+          {/* Práticas Agricolas */}
           <Route path="praticas-agricolas" element={<PraticasAgricolas />}>
+            {/* Rota de redirecionamento */}
             <Route
               index
               element={
                 <Navigate to="/dashboard/praticas-agricolas/plantios-recomendados" />
               }
             />
+            {/* Rotas de Práticas Agricolas */}
             <Route
               path="plantios-recomendados"
               element={
                 <PlantiosRecomendados onEscolher={handleEscolherPlantio} />
               }
             />
-            <Route
-              path="cadastrar-plantios"
-              element={<CadastrarPlantios />}
-            />
+            <Route path="cadastrar-plantios" element={<CadastrarPlantios />} />
             <Route
               path="cadastrar-plantios/:plantio"
               element={<CadastrarPlantios />}
             />
           </Route>
-          <Route path="mercado/*" element={<Mercado />} />
+
+          {/* Mercado */}
+          <Route path="mercado" element={<Mercado />}>
+            {/* Rota de redirecionamento */}
+            <Route
+              index
+              element={
+                <Navigate to="/dashboard/mercado/vender-plantio" />
+              }
+            />
+            {/* Rotas de Mercado */}
+            <Route path="vender-plantio" element={<VenderPlantio />} />
+            <Route path="ver-plantios" element={<VerPlantios />} />
+          </Route>
         </Routes>
       </div>
     </div>
--- src/pages/PraticasAgricolas/index.jsx
+++ src/pages/PraticasAgricolas/index.jsx
@@ -1,40 +1,29 @@
-// import { useState, useEffect } from "react";
 import { NavLink, Outlet } from "react-router-dom";
-// import PlantiosRecomendados from "./PlantiosRecomendados";
-// import CadastrarPlantios from "./CadastrarPlantios";
 import "./PraticasAgricolas.css";
 
 function PraticasAgricolas() {
-
-
   return (
     <div className="praticas-agricolas">
       <nav className="menu-praticas-agricolas">
-          <NavLink
-            className={({ isActive }) =>
-              [isActive ? "on" : ""].join(" ")
-            }
-            to={"plantios-recomendados"}
-          >
-            Plantios Recomendados
-          </NavLink>
+        <NavLink
+          className={({ isActive }) => [isActive ? "on" : ""].join(" ")}
+          to={"plantios-recomendados"}
+        >
+          Plantios Recomendados
+        </NavLink>
 
-          <NavLink
-            className={({ isActive }) =>
-              [isActive ? "on" : ""].join(" ")
-            }
-            to={"cadastrar-plantios"}
-          >
-            Cadastrar plantios
-          </NavLink>
-
+        <NavLink
+          className={({ isActive }) => [isActive ? "on" : ""].join(" ")}
+          to={"cadastrar-plantios"}
+        >
+          Cadastrar plantios
+        </NavLink>
       </nav>
 
       <div className="praticas-agricolas-conteudo">
         <Outlet />
-      </div>      
+      </div>
     </div>
-
   );
 }
 
