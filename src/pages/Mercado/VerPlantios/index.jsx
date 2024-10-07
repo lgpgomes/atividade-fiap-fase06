165bfd026b46500337836cda18709d5eb6783980
Gabriel Reis <gabrielreiscunha6@gmail.com>
2024-10-07 15:58:53 -0300

    error fixed

--- src/pages/Mercado/VerPlantios/index.jsx
+++ src/pages/Mercado/VerPlantios/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./VerPlantios.css";
 
 function VerPlantios({ plantiosVenda }) {
